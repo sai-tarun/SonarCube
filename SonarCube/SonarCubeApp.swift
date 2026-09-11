f509d08b0f1303852a438278a5dfb41e1c09d483
sai-tarun <tarunsai349@gmail.com>
2026-09-11 23:18:28 +0530

    Initial Commit

---
+++ SonarCube/SonarCubeApp.swift
@@ -0,0 +1,17 @@
+//
+//  SonarCubeApp.swift
+//  SonarCube
+//
+//  Created by Majji  Sai Tarun on 11/09/26.
+//
+
+import SwiftUI
+
+@main
+struct SonarCubeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
